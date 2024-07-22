904e7232b2bf372bdf94ade42cb29cdc0c58fb08
Otakus0320 <jioboyang@gmail.com>
2024-07-22 23:22:53 +0800

    add initialize game-fram menu and initialize game-frame JMenuBar

--- src/io/github/otakus0320/ui/GameJFrame.java
+++ src/io/github/otakus0320/ui/GameJFrame.java
@@ -4,7 +4,43 @@ import javax.swing.*;
 
 public class GameJFrame extends JFrame {
     public GameJFrame() {
-        this.setSize(603,680);
+        // initialize menu
+        initJFrame();
+
+        // initialize JMenuBar
+        initJMenuBar();
+
         this.setVisible(true);
+    }
+
+    private void initJMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu optionJMenu = new JMenu("Option");
+        JMenu aboutJMenu = new JMenu("About");
+
+        JMenuItem restartItem = new JMenuItem("Restart");
+        JMenuItem reLoginItem = new JMenuItem("Sign in again");
+        JMenuItem closeItem = new JMenuItem("Close game");
+
+        JMenuItem accountItem = new JMenuItem("Github link");
+
+        optionJMenu.add(restartItem);
+        optionJMenu.add(reLoginItem);
+        optionJMenu.add(closeItem);
+
+        aboutJMenu.add(accountItem);
+
+        menuBar.add(optionJMenu);
+        menuBar.add(aboutJMenu);
+
+        this.setJMenuBar(menuBar);
+    }
+
+    private void initJFrame() {
+        this.setSize(603,680);
+        this.setTitle("MyGame");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
--- src/io/github/otakus0320/ui/RegisterJFrame.java
+++ src/io/github/otakus0320/ui/RegisterJFrame.java
@@ -5,6 +5,11 @@ import javax.swing.*;
 public class RegisterJFrame extends JFrame {
     public RegisterJFrame() {
         this.setSize(488,500);
+        this.setTitle("RegisterMenu");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         this.setVisible(true);
     }
 }
