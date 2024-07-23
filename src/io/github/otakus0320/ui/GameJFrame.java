61658468a57defb9ef87de530d8e3a7e799f405d
Otakus0320 <jioboyang@gmail.com>
2024-07-23 12:36:26 +0800

    add test img to the game frame

--- src/io/github/otakus0320/ui/GameJFrame.java
+++ src/io/github/otakus0320/ui/GameJFrame.java
@@ -10,7 +10,35 @@ public class GameJFrame extends JFrame {
         // initialize JMenuBar
         initJMenuBar();
 
+        // initialize image
+        initImage();
+
         this.setVisible(true);
+    }
+
+    private void initImage() {
+        // img number
+        int number = 1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                // create ImageIcon
+                ImageIcon icon1 = new ImageIcon("img/testImg/"+number+".png");
+
+                // create JLabel to manage
+                JLabel jlabel = new JLabel(icon1);
+
+                // set location
+                jlabel.setBounds(105*j,105*i,105,105);
+
+                // add JLabel to ui
+                this.getContentPane().add(jlabel);
+
+                // next img
+                number++;
+            }
+        }
+
+
     }
 
     private void initJMenuBar() {
@@ -42,5 +70,6 @@ public class GameJFrame extends JFrame {
         this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLayout(null);
     }
 }
